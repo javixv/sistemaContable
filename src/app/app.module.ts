f8e8553f7262d6a4ec9df88cfabe2f0a3244914f
javixv <javier.fz@outlook.com>
2022-02-11 20:44:41 -0600

    implementacion de nuevos estilos para el tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './shared/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     
